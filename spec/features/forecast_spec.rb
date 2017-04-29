501249690426459319161ea8f9581d45a9c50200
Deepak Vig <deepakvig@Deepaks-MacBook.local>
2017-04-29 13:53:14 +0530

    Ruby version added, Better layout added - bootstrap added, Spec updated for Homepage

--- spec/features/forecast_spec.rb
+++ spec/features/forecast_spec.rb
@@ -2,9 +2,16 @@ require 'rails_helper'
 
 describe 'navigate' do
   describe 'homepage' do
-    it 'can be reached successfully' do
+    before do
       visit root_path
+    end
+
+    it 'can be reached successfully' do
       expect(page.status_code).to eq(200)
+    end
+
+    it 'has a title' do
+      expect(page).to have_content(/Weather Forecast/)
     end
   end
 end
