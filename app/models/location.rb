77dff9aae2a93ef9830dbaa714734d2892589a48
Deepak Vig <deepakvig@Deepaks-MacBook.local>
2017-04-30 17:56:36 +0530

    Location model updated to fetch weather details, specs added

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,5 @@
 class Location
+  include WeatherDetail
   
   # Reference Latitude
   REF_LAT = 0
@@ -7,12 +8,24 @@ class Location
   # for finding coordinates with RANGE 10,000Km
   WITH_IN_METERS = 10000000
 
-  attr_accessor :lat, :lon
+  ATTRS = [:lat, :lon, :city, :country, :temperature, :pressure, :humidity, :min_temperature, :max_temperature, 
+                :clouds, :wind_speed, :wind_angle]
+
+  attr_accessor(*ATTRS)
 
   def initialize(attributes = {})
     attributes.each do |name, value|
       send("#{name}=", value)
     end
+  end
+
+  def weather_detail
+    res = get_weather_details lat: self.lat, lon: self.lon
+    ATTRS.each do |attr|
+      send("#{attr.to_s}=", res[attr])
+    end 
+    self.lat = res[:latitude]
+    self.lon = res[:longitude]
   end
 
   def self.random_geo_coord limit = 5
@@ -24,4 +37,9 @@ class Location
     return locs
   end
 
+  def self.random_geo_coord_with_weather_details limit = 5
+    self.random_geo_coord( limit ).each do |loc|
+      loc.weather_detail
+    end
+  end  
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -7,12 +7,34 @@ RSpec.describe Location, type: :model do
       @location = Location.new
     end
   
-    it "should include the :lat attribute" do
+    it "should include all weather related attributes" do
       expect(@location).to respond_to(:lat)
-    end
-  
-    it "should include the :lon attribute" do
       expect(@location).to respond_to(:lon)
+      expect(@location).to respond_to(:city)
+      expect(@location).to respond_to(:temperature)
+      expect(@location).to respond_to(:pressure)
+      expect(@location).to respond_to(:humidity)
+      expect(@location).to respond_to(:min_temperature)
+      expect(@location).to respond_to(:max_temperature)
+      expect(@location).to respond_to(:clouds)
+      expect(@location).to respond_to(:wind_speed)
+      expect(@location).to respond_to(:wind_angle)
+    end
+  end
+
+  describe "weather_detail" do
+    it "should update location attributes" do
+      @location = Location.new(lat: 0, lon: 0)
+      @location.weather_detail
+      expect(@location.city).to eq("London")
+      expect(@location.temperature).to eq(280.32)
+      expect(@location.pressure).to eq(1012)
+      expect(@location.humidity).to eq(81)
+      expect(@location.min_temperature).to eq(279.15)
+      expect(@location.max_temperature).to eq(281.15)
+      expect(@location.clouds).to eq(90)
+      expect(@location.wind_speed).to eq(4.1)
+      expect(@location.wind_angle).to eq(80)
     end
   end
 
@@ -21,7 +43,11 @@ RSpec.describe Location, type: :model do
       expect(Location).to respond_to(:random_geo_coord)
     end
 
-    describe "should return" do
+    it "should include class method random_geo_coord_with_weather_details" do
+      expect(Location).to respond_to(:random_geo_coord_with_weather_details)
+    end
+
+    describe "should return 'random_geo_coord'" do
       it "locations array" do 
         expect(Location.random_geo_coord.first).to be_an_instance_of(Location)
       end
@@ -32,6 +58,20 @@ RSpec.describe Location, type: :model do
 
       it "locations of length equal to argument passed" do
         expect(Location.random_geo_coord(10).length).to eq(10)
+      end
+    end
+
+    describe "should return 'random_geo_coord_with_weather_details'" do
+      it "locations array" do 
+        expect(Location.random_geo_coord_with_weather_details).to all(be_an_instance_of(Location))
+      end
+
+      it "locations of length 5 when no argument passed" do
+        expect(Location.random_geo_coord_with_weather_details.length).to eq(5)
+      end
+
+      it "locations of length equal to argument passed" do
+        expect(Location.random_geo_coord_with_weather_details(10).length).to eq(10)
       end
     end
   end
